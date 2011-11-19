308489f47660fe7b951fb24933b1ea0b7ec5bb03
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-19 13:40:37 -0800

    Stubbed out VELResponder class to include it in the inheritance hierarchy

---
+++ Framework/Velvet/Velvet/VELResponder.h
@@ -0,0 +1,15 @@
+//
+//  VELResponder.h
+//  Velvet
+//
+//  Created by Justin Spahr-Summers on 19.11.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ * Similar to `UIResponder`, objects of this class respond to and handle events.
+ */
+@interface VELResponder : NSObject
+@end

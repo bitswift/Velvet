73bca2a8018189f929586f25001c88b492ee39cc
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-19 14:15:43 -0800

    Added rootView property to VELNSView, which automatically adds/removes a VELView layer to/from the NSView's layer

--- Framework/Velvet/Velvet/VELNSView.h
+++ Framework/Velvet/Velvet/VELNSView.h
@@ -8,9 +8,15 @@
 
 #import <Cocoa/Cocoa.h>
 
+@class VELView;
+
 /**
  * A layer-hosted `NSView` that is used to host a `VELView` hierarchy. You must
  * use this class to present a `VELView` and allow it to respond to events.
  */
 @interface VELNSView : NSView
+/**
+ * The root view of a `VELView`-based hierarchy to be displayed in the receiver.
+ */
+@property (strong) VELView *rootView;
 @end
--- Framework/Velvet/Velvet/VELNSView.m
+++ Framework/Velvet/Velvet/VELNSView.m
@@ -7,18 +7,65 @@
 //
 
 #import <Velvet/VELNSView.h>
+#import <Velvet/VELView.h>
+
+@interface VELNSView () {
+	/**
+	 * Used to synchronize access to and operations around the `rootView`.
+	 */
+	dispatch_queue_t m_rootViewQueue;
+	
+	VELView *m_rootView;
+}
+
+@end
 
 @implementation VELNSView
+
+#pragma mark Properties
+
+- (VELView *)rootView; {
+	__block VELView *view = nil;
+
+	dispatch_sync(m_rootViewQueue, ^{
+		view = m_rootView;
+	});
+
+	return view;
+}
+
+- (void)setRootView:(VELView *)view; {
+	dispatch_async(m_rootViewQueue, ^{
+		[m_rootView.layer removeFromSuperlayer];
+		m_rootView = view;
+
+		if (view.layer)
+			[self.layer addSublayer:view.layer];
+
+		[self setNeedsDisplay:YES];
+	});
+}
+
+#pragma mark Lifecycle
+
 - (id)initWithFrame:(NSRect)frame; {
     self = [super initWithFrame:frame];
 	if (!self)
 		return nil;
 	
+	m_rootViewQueue = dispatch_queue_create("com.emeraldlark.VELNSView.rootViewQueue", DISPATCH_QUEUE_SERIAL);
+	
 	// set up layer hosting
 	CALayer *layer = [self makeBackingLayer];
 	[self setLayer:layer];
 	[self setWantsLayer:YES];
+
 	return self;
+}
+
+- (void)dealloc {
+	dispatch_release(m_rootViewQueue);
+	m_rootViewQueue = NULL;
 }
 
 @end

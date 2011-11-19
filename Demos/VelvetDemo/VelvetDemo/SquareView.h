248585069c51c3fd93b1ffd4b5dcd64a9b50e934
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-19 14:56:38 -0800

    Implemented a SquareView class in the demo to test -drawRect:

---
+++ Demos/VelvetDemo/VelvetDemo/SquareView.h
@@ -0,0 +1,15 @@
+//
+//  SquareView.h
+//  VelvetDemo
+//
+//  Created by Justin Spahr-Summers on 19.11.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Velvet/VELView.h>
+
+/**
+ * Draws a square.
+ */
+@interface SquareView : VELView
+@end
---
+++ Demos/VelvetDemo/VelvetDemo/SquareView.m
@@ -0,0 +1,21 @@
+//
+//  SquareView.m
+//  VelvetDemo
+//
+//  Created by Justin Spahr-Summers on 19.11.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import "SquareView.h"
+
+@implementation SquareView
+- (void)drawRect:(CGRect)rect {
+	CGContextRef context = [NSGraphicsContext currentContext].graphicsPort;
+
+	CGColorRef color = CGColorCreateGenericRGB(1, 0, 0, 1);
+	CGContextSetFillColorWithColor(context, color);
+	CGColorRelease(color);
+
+	CGContextFillRect(context, self.bounds);
+}
+@end

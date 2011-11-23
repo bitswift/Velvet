03029444c60dafad40c742e6b41518195c224b48
James Lawton <james.lawton@gmail.com>
2011-11-22 16:36:19 -0800

    VELNSView draws its contained view into its layer.

--- Framework/Velvet/Velvet/VELNSView.m
+++ Framework/Velvet/Velvet/VELNSView.m
@@ -12,6 +12,7 @@
 #import <Velvet/VELContext.h>
 #import <Velvet/VELViewPrivate.h>
 
+
 @implementation VELNSView
 
 #pragma mark Properties
@@ -32,22 +33,40 @@
 	[view setWantsLayer:YES];
 
 	dispatch_sync_recursive(self.context.dispatchQueue, ^{
+		if (view != m_NSView) [m_NSView removeFromSuperview];
+
 		view.frame = [self.superview convertRect:self.frame toView:self.hostView.rootView];
 
-		[self.hostView addSubview:view];
-		m_NSView = view;
+		if (view != m_NSView) {
+			[self.hostView addSubview:view];
+			m_NSView = view;
+		}
 	});
 }
 
 #pragma mark Lifecycle
 
+- (id)init {
+	self = [super init];
+	if (!self)
+		return nil;
+
+	return self;
+}
+
 - (id)initWithNSView:(NSView *)view; {
 	self = [self init];
 	if (!self)
 		return nil;
-	
+
 	self.NSView = view;
 	return self;
+}
+
+#pragma mark CALayer delegate
+
+- (void)displayLayer:(CALayer *)layer {
+	layer.contents = self.NSView.layer.contents;
 }
 
 @end

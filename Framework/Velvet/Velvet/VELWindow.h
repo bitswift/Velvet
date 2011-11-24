2f1bfff823cd23407d40ccd800e0b6eba9e56dac
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-23 16:44:22 -0800

    Documented the purpose of the VELWindow class

--- Framework/Velvet/Velvet/VELWindow.h
+++ Framework/Velvet/Velvet/VELWindow.h
@@ -8,6 +8,12 @@
 
 #import <AppKit/AppKit.h>
 
+/**
+ * An `NSWindow` with support for dispatching events to `VELView` hierarchies
+ * contained within. You must use an instance of this class in order for events
+ * to be received correctly by Velvet.
+ *
+ * The `contentView` of this window must be an `NSVelvetView`.
+ */
 @interface VELWindow : NSWindow
-
 @end

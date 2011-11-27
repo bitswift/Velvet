fe6f0e90085f6b309f065effb2c7e90d272d7639
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-26 17:05:51 -0800

    Discard appledoc's exit status -- always consider documentation generation a success
    Reviewed by Josh Vera.

--- Documentation/generate_documentation.sh
+++ Documentation/generate_documentation.sh
@@ -5,11 +5,4 @@
 # modification.
 
 appledoc -o Documentation --explicit-crossref --no-repeat-first-par --no-search-undocumented-doc --project-name Velvet --project-company "Emerald Lark" "$@" Framework/Velvet/Velvet
-
-# AppleDoc's exit status is 1 on success
-if [ "$?" -ne 1 ]
-then
-    exit 1
-else
-    exit 0
-fi
+exit 0

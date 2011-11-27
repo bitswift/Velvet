a2f60beef7c424a14fd01e9e99bd806afa85a4f5
Josh Vera <josh@joshvera.com>
2011-11-26 19:06:55 -0800

    Made VELNSViewPrivate.h a header completely private to the framework, and documented the interface
    Reviewed by Justin.

--- Framework/Velvet/Velvet/VELNSViewPrivate.h
+++ Framework/Velvet/Velvet/VELNSViewPrivate.h
@@ -8,6 +8,15 @@
 
 #import <Velvet/VELNSView.h>
 
+/*
+ * Private functionality of <VELNSView> that needs to be exposed to other parts
+ * of the framework.
+ */
 @interface VELNSView (Private)
+/*
+ * Whether the receiver should render its `NSView`.
+ *
+ * If set to `NO`, the `NSView` is rendered once and cached into the receiver's layer. This is typically used for animations.
+ */
 @property (nonatomic, assign) BOOL rendersContainedView;
 @end

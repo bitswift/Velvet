c0c081de58bb76162fc4567042627d134851d511
James Lawton <james.lawton@gmail.com>
2011-11-28 16:08:36 -0800

    Implemented AppKit and Velvet animation demos.

---
+++ Demos/AppKitAnimationDemo/AppKitAnimationDemo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AppKitAnimationDemo
+//
+//  Created by James Lawton on 11/28/11.
+//  Copyright (c) 2011 Übermind, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}

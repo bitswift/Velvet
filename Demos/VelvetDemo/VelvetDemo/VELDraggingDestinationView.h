2f23ca2db5f595e66a8ca9f0b2a7a359d56b86d6
Josh Vera <josh@joshvera.com>
2011-11-29 13:34:30 -0800

    Adding stub VELDraggingDestinationView

---
+++ Demos/VelvetDemo/VelvetDemo/VELDraggingDestinationView.h
@@ -0,0 +1,14 @@
+//
+//  VELDraggingDestinationView.h
+//  VelvetDemo
+//
+//  Created by Josh Vera on 11/29/11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Velvet/VELView.h>
+#import <AppKit/NSDragging.h>
+
+@interface VELDraggingDestinationView : VELView <NSDraggingDestination>
+
+@end
---
+++ Demos/VelvetDemo/VelvetDemo/VELDraggingDestinationView.m
@@ -0,0 +1,13 @@
+//
+//  VELDraggingDestinationView.m
+//  VelvetDemo
+//
+//  Created by Josh Vera on 11/29/11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import "VELDraggingDestinationView.h"
+
+@implementation VELDraggingDestinationView
+
+@end

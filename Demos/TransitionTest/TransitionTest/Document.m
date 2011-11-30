8c8f1b57e13d2281c552f1db54a68aabe311cc73
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-30 15:34:23 -0800

    Fix project copyrights

--- Demos/TransitionTest/TransitionTest/Document.m
+++ Demos/TransitionTest/TransitionTest/Document.m
@@ -3,7 +3,7 @@
 //  TransitionTest
 //
 //  Created by Justin Spahr-Summers on 30.11.11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
 //
 
 #import "Document.h"

9bb849b1b56008e3157f387b619960c30b95beb3
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-01 14:14:35 -0800

    Implemented NavigationBar

---
+++ Demos/TransitionTest/TransitionTest/NavigationBar.h
@@ -0,0 +1,14 @@
+//
+//  NavigationBar.h
+//  TransitionTest
+//
+//  Created by Justin Spahr-Summers on 01.12.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Velvet/Velvet.h>
+#import "RotatingControl.h"
+
+@interface NavigationBar : VELView
+@property (nonatomic, strong) RotatingControl *openCloseButton;
+@end

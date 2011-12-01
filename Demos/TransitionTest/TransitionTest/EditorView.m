a54b24f76cdaa1a501c21c027dce97f813613f0c
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-01 14:04:55 -0800

    Implemented EditorView

--- Demos/TransitionTest/TransitionTest/EditorView.m
+++ Demos/TransitionTest/TransitionTest/EditorView.m
@@ -18,14 +18,39 @@
         return nil;
 
     self.screenView = [[VELImageView alloc] init];
+    self.screenView.bounds = CGRectMake(0, 0, 320, 480);
     [self addSubview:self.screenView];
 
     self.deviceFrame = [[VELImageView alloc] init];
-    self.deviceFrame.image = [NSImage imageNamed:@"device.png"].CGImage;
+    self.deviceFrame.image = [[NSImage imageNamed:@"device.png"] CGImage];
     self.deviceFrame.alpha = 0;
     [self addSubview:self.deviceFrame];
 
     return self;
+}
+
+- (void)drawRect:(CGRect)rect {
+    NSGradient *grad = [[NSGradient alloc] initWithStartingColor:[NSColor grayColor] endingColor:[NSColor blackColor]];
+    [grad drawInRect:self.bounds angle:-90.0f];
+}
+
+- (void)layoutSubviews {
+    BOOL hasImage = (self.screenView.image != nil);
+
+    self.screenView.hidden = !hasImage;
+    self.screenView.center = self.center;
+
+    self.deviceFrame.hidden = !hasImage;
+    self.deviceFrame.center = self.center;
+}
+
+- (void)editScreen:(Screen *)screen {
+    self.screenView.image = screen.thumbnailImageView.image;
+    [self setNeedsLayout];
+    
+    [VELView animateWithDuration:0.5 animations:^{
+        self.deviceFrame.alpha = 1;
+    }];
 }
 
 @end

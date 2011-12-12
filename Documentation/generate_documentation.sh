bbcf71ca5f14c20b708fd729358928ab16ddccba
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-12 12:55:17 -0800

    Fix how the generate_documentation script determines the name of the current project (no more 'workspace' names when generated by Jenkins)
    Reviewed by Josh.

--- Documentation/generate_documentation.sh
+++ Documentation/generate_documentation.sh
@@ -4,7 +4,7 @@
 # documentation. Any arguments on the command line are passed through without
 # modification.
 
-PROJECT_NAME=`basename "$PWD"`
+PROJECT_NAME=`ls | grep -m 1 *.xc* | awk -F. '{ print $1 }'`
 
 appledoc -o Documentation --explicit-crossref --no-repeat-first-par --no-search-undocumented-doc --project-company "Emerald Lark" --project-name "$PROJECT_NAME" "$@"
 exit 0

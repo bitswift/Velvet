1e8e2a983f16a3aa1d1e5650b7b21e5227b2f726
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-21 22:52:45 -0800

    Defined VELViewController interface

---
+++ Frameworks/Velvet/Velvet/VELViewController.m
@@ -0,0 +1,16 @@
+//
+//  VELViewController.m
+//  Velvet
+//
+//  Created by Justin Spahr-Summers on 21.12.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Velvet/VELViewController.h>
+#import <Velvet/VELView.h>
+
+@interface VELViewController ()
+@end
+
+@implementation VELViewController
+@end

958441c2a4477b23f6dbc8ab8f6a2ac8698110c0
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-03 10:48:31 -0800

    Wrote unit tests for VELWindow

---
+++ Frameworks/Velvet/VelvetTests/VELWindowTests.m
@@ -0,0 +1,50 @@
+//
+//  VELWindowTests.m
+//  Velvet
+//
+//  Created by Justin Spahr-Summers on 03.01.12.
+//  Copyright (c) 2012 Emerald Lark. All rights reserved.
+//
+
+#import "VELWindowTests.h"
+#import <Velvet/Velvet.h>
+#import <Cocoa/Cocoa.h>
+
+@interface VELWindowTests ()
+- (VELWindow *)newWindow;
+@end
+
+@implementation VELWindowTests
+
+- (VELWindow *)newWindow {
+    return [[VELWindow alloc]
+        initWithContentRect:CGRectMake(100, 100, 500, 500)
+        styleMask:NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask
+        backing:NSBackingStoreBuffered
+        defer:NO
+        screen:nil
+    ];
+}
+
+- (void)testInitialization {
+    VELWindow *window = [self newWindow];
+    STAssertNotNil(window, @"");
+}
+
+- (void)testContentView {
+    VELWindow *window = [self newWindow];
+
+    NSVelvetView *hostView = window.contentView;
+    STAssertNotNil(hostView, @"");
+    STAssertTrue([hostView isKindOfClass:[NSVelvetView class]], @"");
+}
+
+- (void)testRootView {
+    VELWindow *window = [self newWindow];
+    
+    VELView *rootView = window.rootView;
+    STAssertNotNil(rootView, @"");
+    STAssertEquals(rootView.hostView, window.contentView, @"");
+}
+
+@end

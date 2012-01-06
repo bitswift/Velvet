b6e0f86d2693dc0fa14316b6945b8f69117f6081
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-06 14:35:20 -0800

    Added NSEqualEdgeInsets() method, to compare two NSEdgeInsets structures

---
+++ Frameworks/Velvet/VelvetTests/VELAppKitAdditionsTests.m
@@ -0,0 +1,25 @@
+//
+//  VELAppKitAdditionsTests.m
+//  Velvet
+//
+//  Created by Justin Spahr-Summers on 06.01.12.
+//  Copyright (c) 2012 Emerald Lark. All rights reserved.
+//
+
+#import "VELAppKitAdditionsTests.h"
+#import <Cocoa/Cocoa.h>
+#import <Velvet/Velvet.h>
+
+@implementation VELAppKitAdditionsTests
+
+- (void)testNSEqualEdgeInsets {
+    NSEdgeInsets insets = NSEdgeInsetsMake(2, 3, 4, 5);
+
+    NSEdgeInsets equalInsets = NSEdgeInsetsMake(2, 3, 4, 5);
+    STAssertTrue(NSEqualEdgeInsets(insets, equalInsets), @"");
+
+    NSEdgeInsets inequalInsets = NSEdgeInsetsMake(0, 0, 4, 5);
+    STAssertFalse(NSEqualEdgeInsets(insets, inequalInsets), @"");
+}
+
+@end

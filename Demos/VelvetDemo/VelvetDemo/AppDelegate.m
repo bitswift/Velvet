b0a2a4e6a68f050793db852a90fd7e5a7896e48c
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-09 11:13:44 -0800

    Revert "In the demo, work around INTERNAL-437 by not using -addSubview:, and instead modifying VELScrollView's subviews only through the 'subviews' property"
    This reverts commit 16cde65b301753d157bc8c2620acfa015a580445.
    
    This breaks the demo, but the rest of the code on the branch has been verified as working.

--- Demos/VelvetDemo/VelvetDemo/AppDelegate.m
+++ Demos/VelvetDemo/VelvetDemo/AppDelegate.m
@@ -147,7 +147,7 @@
     VELView *rootSquareView = [[SquareView alloc] init];
     rootSquareView.frame = CGRectMake(60, 60, 70, 70);
     rootSquareView.layer.opacity = 0.8f;
-    self.scrollView.subviews = [self.scrollView.subviews arrayByAddingObject:rootSquareView];
+    [self.scrollView addSubview:rootSquareView];
 
     VELNSView *textFieldHost = [[VELNSView alloc] initWithNSView:textField];
     imageView.subviews = [imageView.subviews arrayByAddingObject:textFieldHost];

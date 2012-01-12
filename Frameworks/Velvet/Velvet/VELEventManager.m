4ee7f90defbf706c80e913baa4b0c901ad6fb942
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-11 17:27:46 -0800

    Don't check for protocol conformance in VELEventManager dispatch, since categories don't reliably add protocols

--- Frameworks/Velvet/Velvet/VELEventManager.m
+++ Frameworks/Velvet/Velvet/VELEventManager.m
@@ -148,7 +148,7 @@
         case NSOtherMouseUp:
         case NSOtherMouseDragged: {
             id responder = [theEvent.window firstResponder];
-            if (![responder isKindOfClass:[NSView class]] && [responder conformsToProtocol:@protocol(VELBridgedView)]) {
+            if (![responder isKindOfClass:[NSView class]] && [responder respondsToSelector:@selector(descendantViewAtPoint:)]) {
                 [self dispatchEvent:theEvent toResponder:responder];
             }
 

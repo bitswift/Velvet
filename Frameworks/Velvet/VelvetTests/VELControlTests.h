f74ca84940490a8b9b5425f7e79bc96498ada2f0
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-16 16:46:34 -0800

    Unit tests for VELControl, including the new selection/deselection events

---
+++ Frameworks/Velvet/VelvetTests/VELControlTests.h
@@ -0,0 +1,15 @@
+//
+//  VELControlTests.h
+//  Velvet
+//
+//  Created by Justin Spahr-Summers on 16.01.12.
+//  Copyright (c) 2012 Bitswift. All rights reserved.
+//
+
+//  Application unit tests contain unit test code that must be injected into an application to run correctly.
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface VELControlTests : SenTestCase
+
+@end

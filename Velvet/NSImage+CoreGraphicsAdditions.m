6e89e1b1df9e50b07ed29ba16714a50fc77b3db5
Josh Vera <josh@joshvera.com>
2012-01-19 10:59:20 -0800

    Fix errors occurring when trying to obtain the graphics context for the first window in NSImage.CGImage

--- Velvet/NSImage+CoreGraphicsAdditions.m
+++ Velvet/NSImage+CoreGraphicsAdditions.m
@@ -12,13 +12,6 @@
 @safecategory (NSImage, CoreGraphicsAdditions)
 - (CGImageRef)CGImage; {
     NSGraphicsContext *graphicsContext = [NSGraphicsContext currentContext];
-    if (!graphicsContext) {
-        NSArray *windows = [NSApp windows];
-        if ([windows count]) {
-            graphicsContext = [[windows objectAtIndex:0] graphicsContext];
-        }
-    }
-
     return [self CGImageForProposedRect:NULL context:graphicsContext hints:nil];
 }
 

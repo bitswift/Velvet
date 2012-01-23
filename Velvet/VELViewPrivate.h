e6fd11c3b4a6c2d842c8afa0bf8934bb34e02466
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-22 16:47:32 -0800

    Moved +[VELView doesCustomDrawing] method into VELViewPrivate, so VELViewLayer can access it

--- Velvet/VELViewPrivate.h
+++ Velvet/VELViewPrivate.h
@@ -11,13 +11,22 @@
 
 @class VELViewController;
 
-/*
+/**
  * Private functionality of <VELView> that needs to be exposed to other parts of
  * the framework.
  */
 @interface VELView (VELViewPrivate)
-/*
+/**
  * A reference to the view controller which is managing the receiver.
  */
 @property (nonatomic, weak) VELViewController *viewController;
+
+/**
+ * Whether this view class does its own drawing, as determined by the
+ * implementation of a <drawRect:> method.
+ *
+ * If this is `YES`, a bitmap context is automatically created for drawing, and
+ * the results of any drawing are cached in its layer.
+ */
++ (BOOL)doesCustomDrawing;
 @end

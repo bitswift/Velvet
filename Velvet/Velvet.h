54a93b5b0be75481071fb00b2e68dc124827d18f
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-02-19 20:44:00 -0800

    Added VELNSViewController, which functions like a VELViewController, and can automatically load an NSView from a nib

--- Velvet/Velvet.h
+++ Velvet/Velvet.h
@@ -23,6 +23,7 @@
 #import <Velvet/VELImageView.h>
 #import <Velvet/VELLabel.h>
 #import <Velvet/VELNSView.h>
+#import <Velvet/VELNSViewController.h>
 #import <Velvet/VELView.h>
 #import <Velvet/VELViewController.h>
 #import <Velvet/VELWindow.h>

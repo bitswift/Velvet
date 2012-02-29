0fd18c9f49aa779cece4092d002ca22913b78ad9
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-02-29 13:51:04 -0800

    Clarify that -scrollToPoint: and -scrollToIncludeRect: have unspecified behavior when the point or rectangle could result in odd scrolling

--- Velvet/VELScrollView.h
+++ Velvet/VELScrollView.h
@@ -22,19 +22,24 @@
  * Scrolls the receiver such that the visible rectangle originates at the given
  * point. This method should enable any applicable animations.
  *
+ * If scrolling to the given point would result in a rectangle extending past
+ * the scroll view's content, the behavior is unspecified; however, the
+ * resulting visible rectangle should still include the given point.
+ *
  * @param point A point, specified in the receiver's coordinate system, that
  * should be the new origin for the scroll view's content.
  */
 - (void)scrollToPoint:(CGPoint)point;
 
 /**
- * Scrolls the receiver the minimum amount required to ensure that the given
+ * Scrolls the receiver the minimum distance required to ensure that the given
  * rectangle is made visible. This method should enable any applicable
  * animations.
  *
  * If the given rectangle is already visible in the scroll view, nothing
  * happens. If the rectangle is larger than the size of the scroll view, as much
- * of it as possible should be made visible.
+ * of the rectangle as possible should be made visible; however, it is
+ * unspecified which part of the rectangle will be used.
  *
  * This method is named to avoid method signature conflicts with AppKit.
  *

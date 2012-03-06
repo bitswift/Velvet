09cbe12a138d361b9c04784579bdb6ebf6edb5ca
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-03-06 14:46:55 -0800

    Changed -[VELEventRecognizer addActionUsingBlock:] to accept a block that takes any kind of object, to make it easy to have blocks that accept subclasses

--- Velvet/VELEventRecognizer.h
+++ Velvet/VELEventRecognizer.h
@@ -255,7 +255,7 @@ typedef enum {
  * @param block A block to invoke when the receiver's <state> changes. The block
  * will be passed the receiver when invoked.
  */
-- (id)addActionUsingBlock:(void (^)(VELEventRecognizer *))block;
+- (id)addActionUsingBlock:(void (^)(id recognizer))block;
 
 /**
  * De-registers a block previously added with <addActionUsingBlock:>.

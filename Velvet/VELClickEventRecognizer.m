1f19465e100773243da9156d2d1f9be48253599e
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-03-08 10:59:08 -0800

    Add a more useful -description implementation to VELClickEventRecognizer

--- Velvet/VELClickEventRecognizer.m
+++ Velvet/VELClickEventRecognizer.m
@@ -90,4 +90,10 @@
     self.windowClickLocation = CGPointZero;
 }
 
+#pragma mark NSObject overrides
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"<%@: %p>( state = %i, clicks = %u, enabled = %i, view = %@ )", [self class], self, (int)self.state, (unsigned)self.numberOfClicksRequired, (int)self.enabled, self.view];
+}
+
 @end

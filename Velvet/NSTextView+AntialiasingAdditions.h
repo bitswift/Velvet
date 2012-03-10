427190ad9d4e4bcf8e00a1c19f19802a51998031
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-03-10 09:32:22 -0800

    Added a private category on NSTextView that fixes blurry text rendering. WHATCHA GOT NOW, @joshaber?!

---
+++ Velvet/NSTextView+AntialiasingAdditions.h
@@ -0,0 +1,16 @@
+//
+//  NSTextView+AntialiasingAdditions.h
+//  Velvet
+//
+//  Created by Justin Spahr-Summers on 10.03.12.
+//  Copyright (c) 2012 Bitswift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+/**
+ * This private category fixes blurry text in layer-backed text views and text
+ * fields.
+ */
+@interface NSTextView (AntialiasingAdditions)
+@end
---
+++ Velvet/NSTextView+AntialiasingAdditions.m
@@ -0,0 +1,42 @@
+//
+//  NSTextView+AntialiasingAdditions.m
+//  Velvet
+//
+//  Created by Justin Spahr-Summers on 10.03.12.
+//  Copyright (c) 2012 Bitswift. All rights reserved.
+//
+
+#import "NSTextView+AntialiasingAdditions.h"
+#import <objc/runtime.h>
+
+static void (*originalDrawRectIMP)(id, SEL, NSRect);
+
+static void fixedDrawRect (NSTextView *self, SEL _cmd, NSRect rect) {
+    CGContextRef context = [NSGraphicsContext currentContext].graphicsPort;
+
+    CGContextSetAllowsAntialiasing(context, YES);
+    CGContextSetAllowsFontSmoothing(context, YES);
+    CGContextSetAllowsFontSubpixelPositioning(context, YES);
+    CGContextSetAllowsFontSubpixelQuantization(context, YES);
+
+    CGContextSetShouldAntialias(context, YES);
+    CGContextSetShouldSmoothFonts(context, YES);
+    CGContextSetShouldSubpixelPositionFonts(context, YES);
+    CGContextSetShouldSubpixelQuantizeFonts(context, YES);
+
+    // NSTextView likes to fall on non-integral points sometimes -- fix that
+    self.frame = [self.superview backingAlignedRect:self.frame options:NSAlignAllEdgesNearest];
+
+    originalDrawRectIMP(self, _cmd, rect);
+}
+
+@implementation NSTextView (AntialiasingAdditions)
+
++ (void)load {
+    Method drawRect = class_getInstanceMethod(self, @selector(drawRect:));
+    originalDrawRectIMP = (void (*)(id, SEL, NSRect))method_getImplementation(drawRect);
+
+    class_replaceMethod(self, method_getName(drawRect), (IMP)&fixedDrawRect, method_getTypeEncoding(drawRect));
+}
+
+@end

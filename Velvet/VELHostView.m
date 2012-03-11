c288ed27115c90ca73f38fed2ec4159dab3aad1b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-03-10 21:43:33 -0800

    Added a notification definition to <VELHostView> to support 'host view debug mode'

---
+++ Velvet/VELHostView.m
@@ -0,0 +1,12 @@
+//
+//  VELHostView.m
+//  Velvet
+//
+//  Created by Justin Spahr-Summers on 10.03.12.
+//  Copyright (c) 2012 Bitswift. All rights reserved.
+//
+
+#import "VELHostView.h"
+
+NSString * const VELHostViewDebugModeChangedNotification = @"VELHostViewDebugModeChangedNotification";
+NSString * const VELHostViewDebugModeIsEnabledKey = @"VELHostViewDebugModeIsEnabled";
